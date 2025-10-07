be7e3631eb84fdc09247542149f57f97f2ba1546
Family <mh5212357@gmail.com>
2025-10-07 15:15:03 -0400

    I am done step 1, 2, 3, and 4

--- src/main/java/com/pluralsight/CurrentDates.java
+++ src/main/java/com/pluralsight/CurrentDates.java
@@ -1,4 +1,48 @@
 package com.pluralsight;
 
+import javax.swing.text.DateFormatter;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class CurrentDates {
+    public static void main(String[] args) {
+
+      // Current date
+        LocalDate currentDate = LocalDate.now();
+        System.out.println(currentDate);
+
+        System.out.println("============================");
+
+        //Converting the current date to new formate date
+        //Step 1 formating day first
+
+        DateTimeFormatter newFormateDate = DateTimeFormatter.ofPattern( "dd/MM/yyyy");
+        String dayFormate = newFormateDate.format(currentDate);
+        System.out.println(dayFormate);
+
+        System.out.println("============================");
+
+        //Step 2: formating year first
+        //String NewDateInput = "2025-10-07";
+        DateTimeFormatter FormateDateChange = DateTimeFormatter.ofPattern( "yyyy-MM-dd");
+        String yearFormate = FormateDateChange.format(currentDate);
+        System.out.println(yearFormate);
+
+        System.out.println("============================");
+
+      //Step 3: formating month first
+        DateTimeFormatter monthOfformate = DateTimeFormatter.ofPattern("MMMM d, yyyy");
+        String monthFormate = monthOfformate.format(currentDate);
+        System.out.println(monthFormate);
+
+    // Step 4 "E, MMM dd, yyyy HH:mm:ss"
+        DateTimeFormatter dayOfformate = DateTimeFormatter.ofPattern("E, MM dd, yyyy  HH:mm");
+        String fullDayFormate = dayOfformate.format(currentDate);
+        System.out.println(fullDayFormate);
+
+
+
+
+
+    }
 }
